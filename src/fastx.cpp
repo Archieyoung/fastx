900045dde92cae617a4d728aba7845dd2301a4fc
Archie Young <750826700@qq.com>
2021-10-18 11:15:46 +0800

    check commnad

--- src/fastx.cpp
+++ src/fastx.cpp
@@ -1,5 +1,7 @@
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <map>
 #include "getopt.h"
 
 #include "version.hpp"
@@ -24,6 +26,21 @@ int main(int argc, char **argv)
     if (argc == 1) {
         Usage();
         return 0;
+    }
+
+    std::map<std::string, bool> registered_commands = {
+        {"sample", true}
+        };
+    
+    if (registered_commands.find(argv[1]) == registered_commands.end())
+    {
+        if(!registered_commands.at(argv[1])) {
+            std::cerr << "Error! " << argv[1] << " is deprecated!" << std::endl;
+            std::exit(1);
+        }
+    } else {
+        std::cerr << "Error! Unrecognized commond " << argv[1] << std::endl;
+        std::exit(1);
     }
 
     if ( strcmp(argv[1], "sample") == 0 )
